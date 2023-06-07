5ba4d8a6fe406be5b78e8652ba2f6aeadfd987f5
Castro-1 <jecastrog@eafit.edu.co>
2023-06-07 13:49:16 -0500

    layout component created

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,4 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import { useSession, signIn, signOut } from "next-auth/react";
-
-const inter = Inter({ subsets: ["latin"] });
-
+import Layout from "@/components/Layout";
 export default function Home() {
-  const { data: session } = useSession();
-  if (!session) {
-    return (
-      <div className="bg-blue-900 w-screen h-screen flex items-center">
-        <div className="text-center w-full">
-          <button
-            onClick={() => signIn("google")}
-            className="bg-white p-2 px-4 rounded-lg"
-          >
-            Login with Google
-          </button>
-        </div>
-      </div>
-    );
-  }
-  return (
-    <>
-      Signed in as {session.user.email} <br />
-      <button onClick={() => signOut()}>Sign out</button>
-    </>
-  );
+  return <Layout />;
 }
